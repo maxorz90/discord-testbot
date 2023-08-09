92ffe24210bf189f54d4ce991ebd965156465cf1
Max Miermans <max@maxorz.net>
2023-08-10 00:03:09 +0200

    Initial commit with test reply

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { Client, IntentsBitField } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log(`Bot '${c.user.tag}' is online`);
+})
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) {
+    return;
+  }
+  message.reply("understood!");
+  console.log(message);
+})
+
+client.login(process.env.DISCORD_TOKEN);
